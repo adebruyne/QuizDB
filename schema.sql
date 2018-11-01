e6a14f8a4e8812652120b6e4e86b560d25b5dac5
Aylin DeBruyne <adebruyne88@gmail.com>
2018-11-01 14:55:34 -0400

    adds students and answers tables

--- schema.sql
+++ schema.sql
@@ -12,3 +12,14 @@ create table questions (
     quiz_id integer references quizzes(id)
 );
 
+create table students (
+    id serial primary key,
+    name varchar (300)
+);
+
+create table answers (
+    id serial primary key,
+    answer_text text,
+    question_id integer references questions(id),
+    student_id integer references students (id)
+);
