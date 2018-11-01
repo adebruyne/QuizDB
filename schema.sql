bcbf8139d37947733865df45c1b1305e82ec20ed
Aylin DeBruyne <adebruyne88@gmail.com>
2018-11-01 14:47:48 -0400

    create quizzes and questions table

---
+++ schema.sql
@@ -0,0 +1,14 @@
+--create those tables
+
+create table quizzes (
+    id serial primary key,
+    name varchar (300)
+);
+
+create table questions (
+    id serial primary key,
+    question_text text,
+    correct_answer text,
+    quiz_id integer references quizzes(id)
+);
+
